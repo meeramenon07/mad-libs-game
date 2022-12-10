65e9d3c67cc2a1a4105d371f1ae7f76afda1e729
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2022-12-10 21:14:01 +0800

    create game.py

---
+++ game.py
@@ -0,0 +1,24 @@
+your_name = input("Enter your name: ")
+number = input("Enter a number: ")
+adjective = input("Enter an adjective: ")
+color = input("Enter a color: ")
+noun = input("Enter a noun: ")
+plural_food = input("Enter a plural food: ")
+verb_ing = input("Enter a verb ending with ing: ")
+clothing = input("Enter a clothing: ")
+adjective = input("Enter an adjective: ")
+celebrity_name = input("Enter a celebrity name: ")
+person_name = input("Enter a person's name: " )
+occupation = input("Enter an occupation:  ")
+
+print("My name is " + your_name + "and I am " + number + " years old.")	
+print("If I were President,  I'd do a whole bunch of   " + adjective + " things.")
+print("1. I would drive the biggest " + color + "car in the country.  And that car would go faster than any other  "  + noun + " in the world! ")
+
+print("2. Everyone  would eat Pepperoni " + plural_food  +  "for dinner. ")
+
+print(" 3. I would live in the statue of " + noun + " and build a " + verb_ing + " pool at her feet. ")
+
+print("4. I would wear a/an " + clothing + " on my head,  and everyone would say I look " + adjective + " like  " + celebrity_name)
+
+print("5. Schools would be open only " + number + " days a year. ")
